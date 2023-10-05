963dce285f14695dda77f37e9559c636d5e55380
Jonathan Oliver <jonoliver1997@gmail.com>
2023-10-04 20:47:48 -0700

    set up database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 // Desc: Entry point for the API
-
+import mongoose from "mongoose";
 import express from "express";
+import dotenv from "dotenv";
+import { TransactionModel } from "../api/models/Transaction.js";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
@@ -8,10 +12,25 @@ app.get("/api/test", (req, res) => {
   res.json({ message: "Hello World" });
 });
 
-app.post("/api/transaction", (req, res) => {
-  res.json(req.body);
+app.post("/api/transaction", async (req, res) => {
+  await mongoose.connect(process.env.MONGO_URL);
+  const { name, price, description, datetime } = req.body;
+  const transaction = await TransactionModel.create({
+    name,
+    price,
+    description,
+    datetime,
+  });
+  res.json(transaction);
+});
+
+app.get("/api/transactions", async (req, res) => {
+  await mongoose.connect(process.env.MONGO_URL);
+  const transactions = await TransactionModel.find();
+  res.json(transactions);
 });
 
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
 });
+//GHgvUouBPOuI8A6u
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { set } from "mongoose";
 
 function App() {
   const [name, setName] = useState("");
   const [datetime, setDatetime] = useState("");
   const [description, setDescription] = useState("");
+  const [price, setPrice] = useState(0);
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    getTransactions().then((transactions) => {
+      setTransactions(transactions);
+    });
+  }, [transactions]);
+
+  async function getTransactions() {
+    const url = import.meta.env.VITE_REACT_APP_API_URL + "/transactions";
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
+  }
 
   function addNewTransaction(e) {
     e.preventDefault();
@@ -16,18 +32,33 @@ function App() {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ name, description, datetime }),
+      body: JSON.stringify({ name, price, description, datetime }),
     }).then((response) => {
       response.json().then((json) => {
+        setName("");
+        setDatetime("");
+        setPrice(0);
+        setDescription("");
         console.log("result", json);
       });
     });
   }
 
+  let balance = 0;
+  transactions.forEach((transaction) => {
+    balance += transaction.price;
+  });
+
+  balance = balance.toFixed(2);
+  const dollars = balance.split(".")[0];
+  const cents = balance.split(".")[1];
+  const formattedDate = datetime.split("T")[0];
+
   return (
     <main>
       <h1>
-        $400<span>.00</span>
+        ${dollars}
+        <span>{cents}</span>
       </h1>
       <form onSubmit={addNewTransaction}>
         <div className="basic-info">
@@ -36,12 +67,21 @@ function App() {
             placeholder="Name"
             value={name}
             onChange={(e) => setName(e.target.value)}
+            required
+          />
+          <input
+            type="text"
+            placeholder="Price"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            required
           />
           <input
             type="datetime-local"
             placeholder="Date"
             value={datetime}
             onChange={(e) => setDatetime(e.target.value)}
+            required
           />
         </div>
         <div className="description">
@@ -50,41 +90,33 @@ function App() {
             placeholder="Description"
             value={description}
             onChange={(e) => setDescription(e.target.value)}
+            required
           />
         </div>
         <button type="submit">Add Transaction</button>
       </form>
       <div className="all--transactions">
-        <div className="transaction">
-          <div className="left">
-            <div className="name">New Iphone</div>
-            <div className="description">Phone broke</div>
-          </div>
-          <div className="right">
-            <div className="price red">-$500</div>
-            <div className="datetime">2022-12-18 15:45</div>
-          </div>
-        </div>
-        <div className="transaction">
-          <div className="left">
-            <div className="name">New Samsung TV</div>
-            <div className="description">Time for new TV</div>
-          </div>
-          <div className="right">
-            <div className="price red">-$500</div>
-            <div className="datetime">2022-12-18 15:45</div>
-          </div>
-        </div>
-        <div className="transaction">
-          <div className="left">
-            <div className="name">Gig Job</div>
-            <div className="description">Time for new TV</div>
-          </div>
-          <div className="right">
-            <div className="price green">+$500</div>
-            <div className="datetime">2022-12-18 15:45</div>
-          </div>
-        </div>
+        {transactions.length > 0 &&
+          transactions.map((transaction) => (
+            <div className="transaction">
+              <div className="left">
+                <div className="name">{transaction.name}</div>
+                <div className="description">{transaction.description}</div>
+              </div>
+              <div className="right">
+                <div
+                  className={
+                    "price " + (transaction.price < 0 ? "red" : "green")
+                  }
+                >
+                  {transaction.price}
+                </div>
+                <div className="datetime">
+                  {transaction.datetime.split("T")[0]}
+                </div>
+              </div>
+            </div>
+          ))}
       </div>
     </main>
   );
